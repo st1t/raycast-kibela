fd3d0886790b5be17f5f3fd673df048a7ee5a956
Shota Ito <shota.ito.jp@gmail.com>
2022-04-10 19:15:46 +0900

    Execute npm run fix-lint

--- src/command-list-notes.tsx
+++ src/command-list-notes.tsx
@@ -1,22 +1,22 @@
-import {Action, ActionPanel, List} from "@raycast/api";
-import {useNotes} from "./useNotes";
+import { Action, ActionPanel, List } from "@raycast/api";
+import { useNotes } from "./useNotes";
 
 export default function CommandListNotes() {
-    const {notes, notesAreLoading} = useNotes();
-    return (
-        <List isLoading={notesAreLoading}>
-            {notes.map(value => (
-                <List.Item
-                    key={value[0]}
-                    icon={{source: {light: "lion-icon.png", dark: "lion-icon.png"}}}
-                    title={"【" + value[3] + "】" + value[1]}
-                    actions={
-                        <ActionPanel>
-                            <Action.OpenInBrowser url={value[2]}/>
-                        </ActionPanel>
-                    }
-                />
-            ))}
-        </List>
-    );
+  const { notes, notesAreLoading } = useNotes();
+  return (
+    <List isLoading={notesAreLoading}>
+      {notes.map((value) => (
+        <List.Item
+          key={value[0]}
+          icon={{ source: { light: "lion-icon.png", dark: "lion-icon.png" } }}
+          title={"【" + value[3] + "】" + value[1]}
+          actions={
+            <ActionPanel>
+              <Action.OpenInBrowser url={value[2]} />
+            </ActionPanel>
+          }
+        />
+      ))}
+    </List>
+  );
 }
--- src/useNotes.ts
+++ src/useNotes.ts
@@ -1,23 +1,22 @@
-import {useEffect, useState} from "react";
-import {listNotes} from "./kibela-api";
+import { useEffect, useState } from "react";
+import { listNotes } from "./kibela-api";
 
 type State = {
-    notesAreLoading: boolean,
-    notes: string[][]
-}
+  notesAreLoading: boolean;
+  notes: string[][];
+};
 
 export const useNotes = () => {
-    const [{notesAreLoading, notes}, setState] = useState<State>({
-        notes: [],
-        notesAreLoading: true,
-    });
+  const [{ notesAreLoading, notes }, setState] = useState<State>({
+    notes: [],
+    notesAreLoading: true,
+  });
 
-    useEffect(() => {
-        listNotes()
-            // .then(value => value || [])
-            .then(notes => setState(prev => ({...prev, notes})))
-            .finally(() => setState(prev => ({...prev, notesAreLoading: false})));
-
-    }, []);
-    return {notes, notesAreLoading};
+  useEffect(() => {
+    listNotes()
+      // .then(value => value || [])
+      .then((notes) => setState((prev) => ({ ...prev, notes })))
+      .finally(() => setState((prev) => ({ ...prev, notesAreLoading: false })));
+  }, []);
+  return { notes, notesAreLoading };
 };
